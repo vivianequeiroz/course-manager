1245b71a753f2c522c81201d1da2cb1143e7d972
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-05-30 16:20:17 -0300

    chore: filtering courses through two-way data binding with  ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CouseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { StarComponent } from './star/star.component';
     StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,13 +8,29 @@ import { CourseService } from './course.service';
 })
 export class CouseListComponent implements OnInit {
 
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+  _filterBy: string;
 
   constructor(
     private courseService: CourseService
   ) { }
 
   ngOnInit(): void {
-    this.courses = this.courseService.retrieveAll();
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+    this.filteredCourses = this._courses.filter(
+      (course: Course) => 
+      course.name.toLowerCase()
+      .indexOf(this._filterBy.toLowerCase()) > -1
+    );
+  }
+
+  get filter() {
+    return this._filterBy;
   }
 }
