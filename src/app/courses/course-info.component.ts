4ded453a12d17992322d8cf75585d4e8250637a1
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-01 17:37:06 -0300

    chore: forms and templates

--- src/app/courses/course-info.component.ts
+++ src/app/courses/course-info.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   templateUrl: './course-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
   
-  courseId: number;
+  course: Course;
 
   constructor(
     private activatedRoute: ActivatedRoute,
+    private courseService: CourseService
   ) {}
 
   ngOnInit(): void {
-   this.courseId = +this.activatedRoute.snapshot.paramMap.get('id');
-   console.log(this.courseId);
+   this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id'));
+   console.log(this.course);
+  }
+
+  save():void{
+    this.courseService.save(this.course);
   }
 }
