49ff3845250fd8968dbe27ec43fa63c344904a32
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-06-05 15:26:13 -0300

    chore: adding common structures but that can have more complexity business rules associated with it into core folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,22 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './shared/component/star/star.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/components/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
       {
         path: '',
